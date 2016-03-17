d847792b4ac0cff5130f3635087aee952b5bcef0
yuqiuyuan <drebander@163.com>
2016-03-17 15:12:33 +0800

    URL类的学习Demo的补充

--- src/demo1/TestURL.java
+++ src/demo1/TestURL.java
@@ -1,16 +1,26 @@
 package demo1;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
+/**
+ * 主要讲述了url对象的三个方法，分别是 url.openStream(); url.openConnection(); url.getContent();
+ */
 public class TestURL {
 	public static void main(String[] args) {
 		try {
 			test4();
+			System.out.println("run test3 infomation:");
+			test3();
+			// System.out.println("run test2 infomation:");
+			// test2();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -30,7 +40,7 @@ public class TestURL {
 			BufferedReader bf = new BufferedReader(isr);
 			String str;
 			while ((str = bf.readLine()) != null) {
-				System.out.println(str);
+				System.out.print(str);
 			}
 
 		} catch (MalformedURLException e) {
@@ -57,6 +67,54 @@ public class TestURL {
 		}
 		Object obj = url.getContent();
 		System.out.println(obj.getClass().getName());
+	}
+
+	/**
+	 * url.openConnection()
+	 * 
+	 * @throws IOException
+	 */
+	public static void test3() throws IOException {
+		URL url = new URL("http://www.baidu.com");
+		// 返回一个 URLConnection对象，他表示到URL所引用的远程对象的连接。
+		URLConnection uc = url.openConnection();
+		// 打开的连接读取的输入流。
+		char[] cbuf = new char[1024];
+		InputStream in = uc.getInputStream();
+		InputStreamReader isr = new InputStreamReader(in);
+		while (isr.read(cbuf) != -1) {
+			System.out.println(cbuf);
+		}
+		in.close();
+	}
+	
+	/**
+	 * 读取URL指定的网页内容 这里 url.openStream()
+	 */
+	public static void test2() throws IOException {
+		URL url = new URL("http://www.baidu.com");
+		// 打开到此 URL 的连接并返回一个用于从该连接读入的InputStream
+		Reader reader = new InputStreamReader(new BufferedInputStream(
+				url.openStream()));
+		int c;
+		while ((c = reader.read()) != -1) {
+			System.out.print((char) c);
+		}
+		reader.close();
+	}
+
+	/**
+	 * 获取URL的输入流，并输出 这里主要说的是 url.openStream() 方法！
+	 */
+	public static void test1() throws IOException {
+		URL url = new URL("http://www.baidu.com");
+		// 打开到此 URL 的连接并返回一个用于从该连接读入的inputstream
+		InputStream in = url.openStream();
+		int c;
+		while ((c = in.read()) != -1) {
+			System.out.println(c);
+		}
+		in.close();
 	}
 
 }
--- src/readme.txt
+++ src/readme.txt
@@ -1,2 +1,7 @@
-java.net.Url URL类的学习
+java.net.Url URL类的学习 2016-03-17
 http://blog.csdn.net/chenzheng_java/article/details/6248090
+2、ClassLoader 类加载器
+Java利用ClassLoader将类装入内存，并且在同一应用中，可以有多个ClassLoader，通过委派机制，把装载的任务传递给上级的装载器的，以次类推，直到启动类装载器(没有上级类装载器)。
+如果启动类装载器能够装载这个类，那么他会首先装载。若果不能，则往下传递。当父类为null时，JVM内置的类(称为:bootstrap class loader)就会充当父类。想想眼下的越来越多的
+使用XML做配置文件或者描述符、部署符。其实这些通过XML文档描述的配置信息最终都要变成java类，其实都是通过ClassLoader来完成的。URLClassLoader是ClassLoader的子类
+它用于从指向JAR文件和目录的URL的搜索路径加载类和资源。也就是说，通过URLClassLoader就可以加载指定jar中的class到内存中。
