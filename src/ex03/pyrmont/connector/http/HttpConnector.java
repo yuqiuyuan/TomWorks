fbe5ee51aa2611ed5756b6dbdbf1de5d46341c46
yuqiuyuan <drebander@163.com>
2016-03-22 14:19:39 +0800

    连接器1

---
+++ src/ex03/pyrmont/connector/http/HttpConnector.java
@@ -0,0 +1,41 @@
+package ex03.pyrmont.connector.http;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpConnector implements Runnable {
+
+	boolean stopped;
+	private String scheme = "http";
+
+	public String getScheme() {
+		return scheme;
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		ServerSocket serverSocket = null;
+		int port = 8080;
+		try {
+			serverSocket = new ServerSocket(port, 1,
+					InetAddress.getByName("127.0.0.1"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		while (!stopped) {
+			// Accept the next incomming connection from the server socket
+			Socket socket = null;
+			try {
+				socket = serverSocket.accept();
+			} catch (Exception e) {
+				continue;
+			}
+			// Hand this socket off to an HttpProcessor
+			// TODO deal thoes things after accept socket
+		}
+	}
+
+}
