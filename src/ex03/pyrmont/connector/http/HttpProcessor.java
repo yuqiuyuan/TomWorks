c9abf1dc6b0d38cd2a3280beef5a92f5496693f5
yuqiuyuan <drebander@163.com>
2016-03-28 14:30:44 +0800

    3.28

--- src/ex03/pyrmont/connector/http/HttpProcessor.java
+++ src/ex03/pyrmont/connector/http/HttpProcessor.java
@@ -1,19 +1,16 @@
 package ex03.pyrmont.connector.http;
 
+import java.io.OutputStream;
 import java.net.Socket;
 
 /*this class used to be called HttpServer*/
 /**
- * HttpProcessor 处理器，在connector中被创建，接受前来的HTTP请求套接字，做下面的事情： 1、创建一个HttpRequest对象。
- * 2、创建一个HttpResponse对象。 3、解析HttpRequest请求的第一行和头部，并放到HttpRequest对象。
- * 4、解析HttpRequest和HttpResponse对象到一个ServletProcessor或者StaticResourceProcessor
- * 。想第二章中说的，ServletProcessor调用被请求的servlet
- * 的service方法，而StaticResourceProcessor发送一个静态资源的内容。
  * 
  * */
 public class HttpProcessor {
 
 	private HttpConnector connector = null;
+	private HttpRequest request;
 	public HttpProcessor(HttpConnector connector) {
 		this.connector = connector;
 	}
@@ -22,6 +19,14 @@ public class HttpProcessor {
 	 * The HttpConnector with which this processor is associated.
 	 */
 	public void process(Socket socket) {
-
+		SocketInputStream input;
+		OutputStream output = null;
+		try {
+			input = new SocketInputStream(socket.getInputStream(), 2048);
+			output = socket.getOutputStream();
+			// create HttpRequest object and parse
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
